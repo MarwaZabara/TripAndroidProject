5981cd38423908f27f27c473fb3d89097f27d268
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2020-03-19 02:34:10 +0200

    add date picker

--- app/src/main/java/com/example/tripandroidproject/View/UnderTest/TestReminder.java
+++ app/src/main/java/com/example/tripandroidproject/View/UnderTest/TestReminder.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.AlarmManager;
+import android.app.DatePickerDialog;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
@@ -11,6 +12,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TimePicker;
 
 import com.example.tripandroidproject.Broadcast.ReminderService.ReminderReceiver;
@@ -18,9 +21,11 @@ import com.example.tripandroidproject.Custom.TimePicker.TimePickerFragment;
 import com.example.tripandroidproject.Presenter.Reminder.ReminderPresenter;
 import com.example.tripandroidproject.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class TestReminder extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+    int hourOfDay = 0; int minute = 0;int year = 0; int month = 0; int dayOfMonth = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +34,62 @@ public class TestReminder extends AppCompatActivity implements TimePickerDialog.
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        Calendar calendar = Calendar.getInstance();
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+//        calendar.set(Calendar.MINUTE, minute);
+//        calendar.set(Calendar.SECOND, 0);
+        TestReminder.this.hourOfDay = hourOfDay;
+        TestReminder.this.minute = minute;
+        if(month > 0) {
+            fillCalenderObj();
+        }
+
+
+//        startAlarmService(calendar);
+    }
+    private void showDateDialog() {
+        final Calendar calendar=Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                calendar.set(Calendar.YEAR,year);
+//                calendar.set(Calendar.MONTH,month);
+//                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+//                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yy-MM-dd");
+                TestReminder.this.year = year;
+                TestReminder.this.month = month;
+                TestReminder.this.dayOfMonth = dayOfMonth;
+                if(hourOfDay > 0 && minute > 0) {
+                    fillCalenderObj();
+                }
+//                date_in.setText(simpleDateFormat.format(calendar.getTime()));
+
+            }
+        };
+
+        new DatePickerDialog(TestReminder.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void fillCalenderObj() {
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(Calendar.YEAR,year);
+        calendar.set(Calendar.MONTH,month);
+        calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
         calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
         calendar.set(Calendar.MINUTE, minute);
         calendar.set(Calendar.SECOND, 0);
+
         ReminderPresenter reminderPresenter = new ReminderPresenter(this);
         reminderPresenter.startReminderService(calendar);
-//        startAlarmService(calendar);
     }
 
 
     public void openTime(View view) {
         DialogFragment timePicker = new TimePickerFragment();
         timePicker.show(getSupportFragmentManager(),"time picker");
+    }
+
+    public void showDatePicker(View view) {
+        showDateDialog();
     }
 }
